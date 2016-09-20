755151e917e9d193c36648c49a935374fdfcea7d
es-lee <dldms800@gmail.com>
2016-09-21 01:12:22 +0900

    client login 뼈대코드 추가

--- client.c
+++ client.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 #define MAX_LEN 140
 #define DUMMY 11111
-
+int login();
 int send_msg();
 int read_msg();
 
@@ -31,31 +31,27 @@ int main (int argc, char **argv) {
 
   connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr));
 
-  //TODO: 로그인
   int user_id = DUMMY;
-  printf("id : ");
-  scanf("%d", &user_id);
-  printf("your id is %d.\n", user_id);
+  login(&user_id);
 
   char cmd;
   char buf [MAX_LEN];
   int dst = DUMMY;
+
   do {
     printf("command : ");
     scanf(" %c", &cmd);
 
-  //TODO: 메시지보내기
+  // 메시지보내기
     if (cmd == 's')
     {
       send_msg(&dst, &buf);
     }
-  //TODO: 메시지읽기
+  // 메시지읽기
     else if (cmd == 'r')
     {
       read_msg(sock_fd, buf);
     }
-
-
   } while (cmd != 'q');
 
 
@@ -63,19 +59,47 @@ int main (int argc, char **argv) {
   return 0;
 }
 
+int login(int * user_id)
+{
+  int login = 1;
+  while (login)
+  {
+    //TODO: 통신부
+    printf("id : ");
+    scanf("%d", user_id);
+    printf("your id is %d.\n", *user_id);
+
+    //기다렸다가 로그인 성공
+    login = 0;
+    printf("Log on success!\n");
+    //실패
+    printf("Log on failed! (invalid ID)\n");
+  }
+  return login;
+}
+
 int send_msg(int * dst, char * buf) {
+  //TODO: 패킷 만들어서 보내기?
 
   printf("R: ");
   scanf("%d", dst);
   printf("M: ");
   scanf("%139s", buf);
   printf("%s\n", buf);
+
+  //TODO: 제대로 보내졌는지 확인하기
+  //서버에서 반응 보내주기 & 타임아웃
+  //타임아웃이면 타임아웃 알려주고 적당히 대응하기
   return 0;
 }
 
 int read_msg(int sock_fd, char * msg) {
-  //TODO: 프로토콜 파싱
-  //메시지 다 받은걸 어떻게 알지? 서버에서 알려주거나 timeout
+  //TODO: 패킷 받기, 파싱
+  //메시지 다 받은걸 어떻게 알지?
+  //1. 서버에서 알려준다
+  //2. 타임아웃
+  //근데 타임아웃을 보내는데서도 체크하면 메시지 받을때는 서버가 다 보낸건지
+  //아니면 그냥 타임아웃 된 건지 어떻게 구분을하지
   int msg_len = read(sock_fd, msg, MAX_LEN-1);
   return 0;
 }
