2afdc92d68b6610f6ba36fdd4e1fa504e8f0ecc6
es-lee <dldms800@gmail.com>
2016-09-22 01:47:23 +0900

    주석 다 지움

--- client.cpp
+++ client.cpp
@@ -24,8 +24,6 @@ int main () {
   server_addr.sin_family = AF_INET;
   // for localhost
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  // for cn.snucse.org
-  //server_addr.sin_addr.s_addr = inet_addr("147.46.240.40");
   server_addr.sin_port = htons(20421);
 
   connect(sock_fd, (struct sockaddr*) &server_addr, sizeof(server_addr));
@@ -33,14 +31,13 @@ int main () {
   int user_id = set_user_id();
   login(user_id, sock_fd);
 
-  char msg [1024];
-  int str_len;
 
   pid_t pid = fork();
   if (pid == 0)
   {
     while(1)
     {
+      char msg [1024];
       fgets(msg, MAX_LEN, stdin);
       if(!strcmp(msg, "q\n"))
       {
@@ -54,37 +51,13 @@ int main () {
   else {
     while(1)
     {
-      str_len = read(sock_fd, msg, MAX_LEN);
+      char msg [1024];
+      int str_len = read(sock_fd, msg, MAX_LEN);
       if(str_len == 0)
         exit(0);
       msg[str_len]=0;
     }
   }
-
-  /*
-  int user_id = DUMMY;
-  login(&user_id);
-
-  char cmd [MAX_LEN];
-  char buf [MAX_LEN];
-  int dst = DUMMY;
-
-  do {
-    printf("command : ");
-    scanf("%s", cmd);
-
-  // 메시지보내기
-    if (strcmp(cmd, "s") == 0)
-    {
-      send_msg(&dst, &buf);
-    }
-  // 메시지읽기
-    else if (strcmp(cmd, "r") == 0)
-    {
-      read_msg(sock_fd, buf);
-    }
-  } while (strcmp(cmd, "q") != 0);
-*/
 
   close(sock_fd);
   return 0;
@@ -108,43 +81,6 @@ std::cout << buf;
   {
     //TODO: 통신부
     write(sock_fd, buf.c_str(), buf.length());
-    //기다렸다가 로그인 성공
-    login = 0;
-    printf("Log on success!\n");
-    //실패
-    printf("Log on failed! (invalid ID)\n");
   }
   return login;
-}
-
-int send_msg(int * dst, char * buf) {
-  //TODO: 패킷 만들어서 보내기?
-
-  printf("R: ");
-  scanf("%d", dst);
-  printf("M: ");
-  scanf("%139s", buf);
-  printf("%s\n", buf);
-
-  //TODO: 제대로 보내졌는지 확인하기
-  //서버에서 반응 보내주기 & 타임아웃
-  //타임아웃이면 타임아웃 알려주고 적당히 대응하기
-  return 0;
-}
-
-int read_msg(int sock_fd, char * msg) {
-  //TODO: 패킷 받기, 파싱
-  //메시지 다 받은걸 어떻게 알지?
-  //1. 서버에서 알려준다
-  //2. 타임아웃
-  //근데 타임아웃을 보내는데서도 체크하면 메시지 받을때는 서버가 다 보낸건지
-  //아니면 그냥 타임아웃 된 건지 어떻게 구분을하지
-  //
-  //위에거 다 치우고. 액티브 된 상태에서 항상 오는 메시지를 받아야하니까 이걸
-  //어떻게 받을지 고민을 해야한다. 스레드를 하나 더 돌리든 프로세스 하나를 더
-  //돌리든. 메시지가 잘 전송된건 tcp가 보장해주는걸로.
-  //일단 돌아가는거 만들고 나서 ack추가할거면 하든가.
-  int msg_len = read(sock_fd, msg, MAX_LEN-1);
-  printf("%d\n",msg_len);
-  return 0;
 }
